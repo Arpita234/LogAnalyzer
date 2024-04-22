5bab7f6a88eba21eec9f69a29f942753153e3943
Arpita <arpita.agrawal123@gmail.com>
2024-04-22 14:33:29 -0700

    Log Analyzer code

---
+++ CustomFileWatcher.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+
+namespace LogAnalyzer
+{
+    public class CustomFileSystemWatcher
+    {
+        private readonly string _folderPath;
+        private readonly string _fileExtension;
+        private DateTime? lastProcessedTime;
+        string lastProcessedFileTime = "Last_Processed_Time.txt";
+        string lastProcessedFilePath = string.Empty;
+
+        public event EventHandler<string>? FileCreated;
+        public CustomFileSystemWatcher(string folderPath, string fileExtension)
+        {
+            _folderPath = folderPath;
+            _fileExtension = fileExtension;
+
+            lastProcessedFilePath = Path.Combine(Directory.GetCurrentDirectory(), lastProcessedFileTime);
+
+
+            lastProcessedTime = LoadLastProcessedTime(lastProcessedFilePath);
+
+
+            if (!Directory.Exists(_folderPath))
+            {
+                throw new DirectoryNotFoundException($"Directory {_folderPath} does not exist.");
+            }
+        }
+
+        public void StartWatching()
+        {
+            try
+            {
+                while (!Console.KeyAvailable)
+                {
+                    string[] files = Directory.GetFiles(_folderPath, $"*.{_fileExtension}");
+                    
+                    // Filter out files that were created before the last processed time
+                    files = files.Where(file => File.GetLastWriteTime(file) > lastProcessedTime).ToArray();
+
+
+                    foreach (string filePath in files)
+                    {                                            
+                       OnFileCreated(filePath);                                          
+                    }
+
+                    if (files.Length > 0)
+                    {
+                        DateTime latestFileTime = files.Max(file => File.GetLastWriteTime(file));
+
+                        // update last procesed time
+                        lastProcessedTime = latestFileTime;
+                        File.WriteAllText(lastProcessedFilePath, latestFileTime.ToString());
+                    }
+                    // Wait for 1 second before checking for new files again
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in StartWatching: {ex.Message}");
+            }
+
+        }
+
+        public DateTime? LoadLastProcessedTime(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string lastProcessedTimeString = File.ReadAllText(filePath);
+                    if (!string.IsNullOrEmpty(lastProcessedTimeString))
+                         return DateTime.Parse(lastProcessedTimeString);
+                }
+                        
+                 return DateTime.MinValue;
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in LoadLastProcessedTime: {ex.Message}");
+                return null;          
+            }
+        }
+
+        protected virtual void OnFileCreated(string filePath)
+        {
+            try
+            {
+                if (FileCreated != null)
+                {
+                    FileCreated?.Invoke(this, filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in OnFileCreated: {ex.Message}");
+            }
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+
+namespace LogAnalyzer
+{
+    class Program
+    {
+
+        private readonly static  object obj = new object();
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string logFolder = @"C:\Logs";
+
+                Console.WriteLine("Log analyzer started. Press any key to exit.");
+
+                /*    
+               // Create a file system watcher to monitor the folder for new log files
+
+                using (FileSystemWatcher watcher = new FileSystemWatcher())
+                {
+                    watcher.Path = logFolder;
+                    watcher.Filter = "*.txt";
+                    watcher.NotifyFilter = NotifyFilters.FileName;
+                    watcher.Created += OnLogFileCreated;
+                    watcher.EnableRaisingEvents = true;
+
+                    Console.WriteLine("Log Analyzer is running. Press any key to exit.");
+                    Console.ReadKey();
+                } 
+                */
+
+                string fileExtension = "txt";
+
+                CustomFileSystemWatcher watcher = new CustomFileSystemWatcher(logFolder, fileExtension);
+                watcher.FileCreated += OnLogFileCreated;
+
+                watcher.StartWatching();
+
+                 Console.WriteLine("CustomFileSystemWatcher is running. Press any key to exit.");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in FileWatcher: {ex.Message}");
+            }
+
+        }
+
+        private static async void OnLogFileCreated(object? sender,string filePath)
+        {
+            try
+            {
+                // Print a message when a new log file is created
+                lock (obj)
+                {
+                    Console.WriteLine($"New log file created: {Path.GetFileName(filePath)}");
+                }
+
+                // Process the newly created log file asynchronously
+                await ProcessLogFileAsync(filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing file - OnLogFileCreated {filePath}: {ex.Message}");
+            }
+
+        }
+
+        private static async Task ProcessLogFileAsync(string filePath)
+        {
+
+            try
+            {
+                if (IsFileReady(filePath)) // Check if the file is ready (not being written to)
+                {
+
+                    string[] keywords = { "warning", "error", "fail" };
+                    Dictionary<string, int> keywordCounts = new Dictionary<string, int>();
+
+                    await using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    using (var reader = new StreamReader(stream))
+                    {
+                        string line;
+                        while ((line = await reader.ReadLineAsync()) != null)
+                        {
+                            foreach (string keyword in keywords)
+                            {
+                                // Split the line into words and check each word for the presence of keywords
+                                string[] words = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                                foreach (string word in words)
+                                {
+                                    if (word.Contains(keyword, StringComparison.OrdinalIgnoreCase))
+                                    {
+
+                                        if (!keywordCounts.ContainsKey(keyword))
+                                        {
+                                            keywordCounts.Add(keyword, 1);
+                                        }
+                                        else
+                                        {
+                                            keywordCounts[keyword] = keywordCounts[keyword] + 1;
+                                        }
+                                    }
+                                }
+                                
+                            }
+                        }
+                    }
+
+                    // Print keyword counts to the console
+                    lock (obj)
+                    {
+                        Console.WriteLine("\n############");
+                        Console.WriteLine($"Summary of occurrences in File: {Path.GetFileName(filePath)}");
+                        foreach (var kvp in keywordCounts)
+                        {
+                            Console.WriteLine($"- '{kvp.Key}': {kvp.Value}");
+                        }
+                        Console.WriteLine("############");
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing log file {filePath}: {ex.Message}");
+            }
+        }
+   
+
+        // Check if a file is ready (not being written to)
+        static bool IsFileReady(string filePath)
+        {
+            try
+            {
+                using (var fileStream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    return true;
+                }
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+        }
+    }
+}
